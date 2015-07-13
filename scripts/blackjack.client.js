6adba8a1a427c099dd2803aecf2e96b335f9a608
Andrew Mealy <andrew.mealy@USSCOAME1-L.LYV.LiveNation.com>
2015-07-13 10:36:42 -0700

    initial commit all files

---
+++ scripts/blackjack.client.js
@@ -0,0 +1,144 @@
+
+var App = {}
+
+App.deal = function () {
+    App.socket.emit('deal');
+}
+App.stand = function () {
+    App.socket.emit('stand');
+}
+/*
+App.stand = function () {
+    App.socket.emit('stand');
+}*/
+
+App.getSuitHtml = function (suit) {
+    var image = 'club.png';
+    if (suit === 'H') {
+        image = 'heart.png';
+    } else if (suit === 'S') {
+        image = 'spade.png';
+    } else if (suit === 'D') {
+        image = 'diamond.png';
+    }
+    return "<img class='card' src='img/" + image + "'/>";
+}
+App.getRankHtml = function (rank) {
+    if (rank === 1) {
+        return 'A';
+    } else if (rank === 11) {
+        return 'J';
+    } else if (rank === 12) {
+        return 'Q';
+    } else if (rank === 13) {
+        return 'K';
+    }
+    return rank;
+}
+App.getCardsHtml = function (cards) {
+    var html = '';
+    for (var i = 0; i < cards.length; i++) {
+
+        var card = cards[i];
+
+        html += App.getRankHtml(card.rank);
+        html += App.getSuitHtml(card.suit);
+    }
+    return html;
+}
+App.updatePlayer = function (player) {
+
+    var html = App.getCardsHtml(player.cards);
+
+    $('#playerCards').html(html);
+    $('#playerScore').text(player.score);
+}
+App.updateDealer = function (dealer) {
+
+    var html = App.getCardsHtml(dealer.cards);
+
+    $('#dealerCards').html(html);
+    $('#dealerScore').text(dealer.score);
+}
+
+App.updateResult = function (result) {
+    var displayResult = result;
+
+    if (result === 'None') {
+        displayResult = '';
+    }
+    $('#result').text(displayResult);
+}
+
+App.disableButton = function (id) {
+
+    $(id).attr('disabled', 'disabled');
+}
+
+App.enableButton = function (id) {
+    $(id).removeAttr('disabled');
+}
+App.disableDeal = function () {
+    App.disableButton('#deal');
+    App.enableButton('#stand');
+}
+App.enableDeal = function () {
+
+    App.enableButton('#deal');
+    App.disableButton('#stand');
+}
+App.enableDealIfGameFinished = function (result) {
+    if (result !== 'None') {
+        App.enableDeal();
+    }
+}
+App.dealResult = function (game) {
+    App.disableDeal();
+
+    App.updateDealer(game.dealer);
+    App.updatePlayer(game.player);
+    App.updateResult(game.result);
+}
+App.standResult = function (game) {
+
+    App.updateDealer(game.dealer);
+    App.updatePlayer(game.player);
+    App.updateResult(game.result);
+
+    App.enableDealIfGameFinished(game.result);
+}
+App.socket = {}
+App.registerClientActions = function () {
+    
+    $('#deal').click(function () {
+
+        App.deal();
+    });
+    $('#stand').click(function () {
+
+        App.stand();
+    });
+}
+App.registerServerActions = function () {    
+    App.socket.on('stand', function (game) {
+
+        App.standResult(game);
+    });
+    App.socket.on('deal', function (game) {
+
+        App.dealResult(game);
+    });
+}
+App.init = function () {
+
+    var socket = io.connect('http://localhost:3000');
+    App.socket = socket;
+    
+    App.registerClientActions();
+    App.registerServerActions();
+    App.enableDeal();
+}
+
+$(document).ready(function () {
+    App.init();
+});
---
+++ server.js
@@ -0,0 +1,80 @@
+var http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    blackjack = require('./lib/blackjack');
+
+var Server = {}
+
+Server.getGame = function (socket, data, callback) {
+    socket.get('game', function (err, game) {
+        callback(socket, game);
+    });
+}
+Server.deal = function (socket, data) {
+    console.log('deal');
+    Server.getGame(socket, data, function (socket, game) {
+        if (!game.isInProgress()) {
+            game.newGame();
+        }
+        socket.emit('deal', game.toJson());
+    });
+}
+Server.stand = function (socket, data) {
+    console.log('stand');
+    Server.getGame(socket, data, function (socket, game) {
+        game.stand();
+        socket.emit('stand', game.toJson());
+    });
+}
+/* hit fucntion
+Server.hit = function (socket, data) {
+    console.log('hit');
+    Server.getGame(socket, data, function (socket, game) {
+        game.hit();
+        socket.emit('hit', game.toJson());
+    });
+}
+*/
+Server.registerSocketIO = function (io) {
+    io.sockets.on('connection', function (socket) {
+        console.log('User connected');
+        socket.set('game', blackjack.newGame())
+
+        socket.on('deal', function (data) {
+            Server.deal(socket, data);
+        });
+        socket.on('stand', function (data) {
+            Server.stand(socket, data);
+        });
+        socket.on('disconnect', function (socket) {
+            console.log('User disconnected');
+        });
+    });
+}
+
+//Found images online with .svg (scalable vector graphics)
+Server.init = function () {
+    var httpServer = http.createServer(function (req, res) {
+        var path = url.parse(req.url).pathname;
+        var contentType = 'text/html';
+        if (path === '/') {
+            path = '/index.html';
+        } else if (path.indexOf('.css')) {
+            contentType = 'text/css';
+        } else if (path.indexOf('.svg')) {
+            contentType = 'image/svg+xml';
+        }
+        fs.readFile(__dirname + path, function (error, data) {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data, 'utf-8');
+        });
+    }).listen(3000);
+    console.log("Listening on port 3000");
+    var io = require('socket.io').listen(httpServer);
+    io.set('log level', 1); //removes console debug info - was annoying
+    Server.registerSocketIO(io);
+}
+
+Server.init();
+
+
