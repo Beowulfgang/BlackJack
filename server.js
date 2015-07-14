6e5e5d852c6fe4ebe545ae12dcd47e0fd4ad7e8e
Andrew Mealy <andrew.mealy@USSCOAME1-L.LYV.LiveNation.com>
2015-07-14 10:31:04 -0700

    removed weird stuff

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function getUsers () {
    return userNames;
 }
 
-<<<<<<< HEAD
+
 
 Server.registerSocketIO = function (io) {
 io.sockets.on('connection', function (socket) {
@@ -56,7 +56,7 @@ io.sockets.on('connection', function (socket) {
         socket.user = user;
         users.push(user);
         updateClients();
-=======
+
 Server.bet = function(socket, data) {
     console.log('bet');
     Server.getGame(socket, data, function(socket, game) {
@@ -85,7 +85,6 @@ Server.registerSocketIO = function (io) {
         socket.on('bet', function(data) {
             Server.bet(socket, data);
         });
->>>>>>> 76106708eafd9f8409e3fdd82d2e2772ac1cd3d7
     });
  //dont touch below
     socket.set('game', blackjack.newGame())
